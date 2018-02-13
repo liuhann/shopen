4e8e464284079d8bc1bfdaa8f583e137944b0a19
liuhan <liuhan@xbniao.com>
2018-02-13 17:07:50 +0800

    Shopen admin console

--- shopen-admin/src/App.vue
+++ shopen-admin/src/App.vue
@@ -2,12 +2,12 @@
   <div id="app" class="ui-app-frame">
     <header>
         <div class="brand">
-          <img src="./assets/shopen.svg"/> <span>SHOPEN</span>
+          <img src="./assets/shopen.svg"/> <span>shopen</span>
         </div>
         <div class="tools"></div>
     </header>
-
     <aside>
+
     </aside>
 
     <main>
@@ -57,10 +57,9 @@ export default {
     min-height: 100%;
 
     header {
-      background-color: #fff;
-      //background-color: #1B5299;
+      /*background-color: #212b35;*/
       box-shadow: rgba(31,53,78,.11) 0 1px;
-      color:@wk;
+      color: #f9f9f9;
       position: fixed;
       z-index: 502;
       top: 0;
@@ -71,13 +70,31 @@ export default {
       height: 5.6rem;
 
       .brand {
-        line-height: 5rem;
-        font-size: 2.5rem;
+        background-color: #1B5299;
+        box-sizing: border-box;
+        display: flex;
+        font-size: 2rem;
+        width: 24rem;
+        align-items: center;
+        height: 100%;
+        min-width: 24rem;
+        padding-right: 0.8rem;
+        padding-left: 1.6rem;
         img {
-          width: 3.8rem;
+          width: 3rem;
           vertical-align: middle;
+        }
+        span {
+          font-weight: bold;
+          font-size: 2.8rem;
           padding-left: 1rem;
         }
+      }
+      .tools {
+        background-color: #1b5299de;
+        height: 100%;
+        display: flex;
+        flex: 1;
       }
     }
 
