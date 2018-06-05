88df374aae63cd3df37825d1cbe194916334ad3d
liuhan <liuhan@xbniao.com>
2018-06-05 16:07:22 +0800

    Shopen admin console

--- packages/file/index.js
+++ packages/file/index.js
@@ -4,13 +4,14 @@ module.exports = {
   name: 'file',
 
   created (app) {
-    app.context.services.fileUpload = new FileService()
+    app.context.services.fileUpload = new FileService(`d:/filestore`)
   },
   
   ready (app) {
     const router = app.context.router
     router.post('/file/upload', async (ctx, next) => {
-      ctx.services.fileUpload.receive(ctx)
+      const fileResult = ctx.services.fileUpload.receive(ctx)
+      ctx.body = fileResult
       await next()
     })
   }
--- packages/file/service.js
+++ packages/file/service.js
@@ -1,4 +1,6 @@
 const makeDir = require('make-dir')
+const fsPromises = require('fs').promises
+const shortid = require('shortid')
 
 class FileService {
   constructor (baseDir) {
@@ -12,10 +14,23 @@ class FileService {
     const fileDir = `${this.baseDir}/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}/${date.getHours()}`
     // create file store dir
     await makeDir(fileDir)
-    if (body.files && body.files.length) {
     
+    const result = []
+    
+    for (const fileName in body.files) {
+      debugger
+      const uploadFile = body.files[fileName]
+      const storePath = `${fileDir}/${shortid.generate()}`
+      await fsPromises.rename(uploadFile.path, storePath)
+      const fileDoc = {
+        path: storePath,
+        name: uploadFile.name,
+        size: uploadFile.size
+      }
+      await this.mongodb.getDb().getCollection('files').insert(fileDoc)
+      result.push(fileDoc)
     }
-    debugger
+    return result
   }
   
   deleteFile () {
