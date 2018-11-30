43b757dca85e213f900888510995d8725f1daeac
liuhan <liuhann@gmail.com>
2018-11-30 16:17:56 +0800

    to bcc

--- packages/danke/templates/joint.js
+++ packages/danke/templates/joint.js
@@ -67,7 +67,7 @@ module.exports = {
       'y': '3vh',
       'width': '90vw',
       'height': '64vh',
-      'style': 'border-top-left-radius: 10px; border-top-right-radius:10px; clip-path:polygon(0 0, 100% 0%, 100% 100%, 0% 90%);',
+      'style': 'border-top-left-radius: 10px; border-top-right-radius:10px; clip-path: polygon(0 0, 100% 0%, 100% 100%, 0% 87%)',
       'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-bottom'},
       'out': {'duration': '500ms', 'animation': 'flip-out-hor-bottom'}
     }, {
@@ -76,7 +76,7 @@ module.exports = {
       'y': '61vh',
       'width': '45vw',
       'height': '35vh',
-      'style': 'border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; clip-path: polygon(0 4%, 100% 14%, 100% 100%, 0% 100%)',
+      'style': 'border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; clip-path: polygon(0 0, 100% 13%, 100% 100%, 0% 100%)',
       'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-top'},
       'out': {'duration': '500ms', 'animation': 'flip-out-hor-top'}
     }, {
@@ -88,6 +88,74 @@ module.exports = {
       'style': 'border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; clip-path: polygon(0 14%, 100% 25%, 100% 100%, 0% 100%)',
       'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-top'},
       'out': {'duration': '500ms', 'animation': 'flip-out-hor-top'}
+    }]
+  }, {
+    'template': 'designed',
+    'display': '',
+    'style': 'background-color: #F5FBFF',
+    'hideDelay': 2000,
+    'triggerClose': 3000,
+    'elements': [{
+      'type': 'image',
+      'x': '5vw',
+      'y': '3vh',
+      'width': '45vw',
+      'height': '39vh',
+      'style': 'border-top-left-radius: 10px; border-top-right-radius:10px; clip-path:polygon(0 0, 100% 0%, 100% 86%, 0% 100%)',
+      'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-top'},
+      'out': {'duration': '500ms', 'animation': 'flip-out-hor-top'}
+    }, {
+      'type': 'image',
+      'x': '53vw',
+      'y': '3vh',
+      'width': '42vw',
+      'height': '39vh',
+      'style': 'border-top-left-radius: 10px; border-top-right-radius:10px; clip-path:polygon(0 0, 100% 0%, 100% 71%, 0% 85%)',
+      'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-top'},
+      'out': {'duration': '500ms', 'animation': 'flip-out-hor-top'}
+    }, {
+      'type': 'image',
+      'x': '5vw',
+      'y': '29vh',
+      'width': '90vw',
+      'height': '66vh',
+      'style': 'border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; clip-path: polygon(0 24%, 100% 7%, 100% 100%, 0% 100%)',
+      'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-bottom'},
+      'out': {'duration': '500ms', 'animation': 'flip-out-hor-bottom'}
+    }]
+  }, {
+    'template': 'designed',
+    'display': '',
+    'style': 'background-color: #F5FBFF',
+    'hideDelay': 2000,
+    'triggerClose': 3000,
+    'elements': [{
+      'type': 'image',
+      'x': '5vw',
+      'y': '3vh',
+      'width': '90vw',
+      'height': '37vh',
+      'style': 'border-top-left-radius: 10px; border-top-right-radius:10px; clip-path:polygon(0 0, 100% 0%, 100% 90%, 0% 81%);',
+      'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-top'},
+      'out': {'duration': '500ms', 'animation': 'flip-out-hor-top'}
+    }, {
+      'type': 'image',
+      'x': '5vw',
+      'y': '35vh',
+      'width': '90vw',
+      'height': '37vh',
+      'style': 'clip-path:polygon(0 0, 100% 10%, 100% 86%, 0% 100%)',
+      'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-bottom'},
+      'out': {'duration': '500ms', 'animation': 'flip-out-hor-bottom'}
+    }, {
+      'type': 'image',
+      'x': '5vw',
+      'y': '64vh',
+      'width': '90vw',
+      'height': '33vh',
+      'style': 'border-bottom-left-radius: 10px; border-bottom-right-radius: 10px; clip-path: polygon(0 32%, 100% 15%, 100% 100%, 0% 100%)',
+      'in': {'duration': '500ms', 'delay': '200ms', 'animation': 'flip-in-hor-bottom'},
+      'out': {'duration': '500ms', 'animation': 'flip-out-hor-bottom'}
     }]
   }]
 }
