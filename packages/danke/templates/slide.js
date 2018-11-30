675fe1abd3de659857887a0bd17617e5e661e2cb
liuhan <liuhann@gmail.com>
2018-11-30 16:18:29 +0800

    to bcc

---
+++ packages/danke/templates/slide.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'id': 'lightcircle',
+  'name': '滑动',
+  'coordinate': 'tl',
+  'desc': '横向切换',
+  'cover': '000/travel.jpg',
+  'scenes': [0, 0, 0],
+  'templates': [{
+    'template': 'designed',
+    'display': '',
+    'hideDelay': 2000,
+    'triggerClose': 3000,
+    'elements': [{
+      'type': 'image',
+      'x': '0vw',
+      'y': '0vh',
+      'width': '100vw',
+      'height': '100vh',
+      'in': {'duration': '500ms', 'animation': 'slide-in-right'},
+      'out': {'duration': '500ms', 'animation': 'slide-out-left-hf'}
+    }]
+  }]
+}
