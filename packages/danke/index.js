f51e2ec5d6e66c7947eb0c5e48f46ff936370731
liuhan <liuhann@gmail.com>
2019-09-10 17:45:04 +0800

    admin control

--- packages/danke/index.js
+++ packages/danke/index.js
@@ -1,45 +1,26 @@
 const RestFulController = require('../rest/restful-controller.js')
 const config = require('../../config.js')
+
+function initRestService (app, db, coll, path) {
+  const router = app.context.router
+  app.context.services[coll + '.rest'] = new RestFulController({
+    router,
+    mongodb: app.context.services.mongodb,
+    dbName: db,
+    coll: coll,
+    path: path,
+    filter: app.middlewares.loginRequired
+  })
+  app.context.services[coll + '.rest'].setAdmin(config.admin)
+}
+
 module.exports = {
   async created (app) {
   },
   ready (app) {
-    const router = app.context.router
-    app.context.services.sceneRest = new RestFulController({
-      router,
-      mongodb: app.context.services.mongodb,
-      dbName: 'danke',
-      coll: 'works',
-      path: '/danke/work',
-      filter: app.middlewares.loginRequired
-    })
-    app.context.services.sceneRest.setAdmin(config.admin)
-    app.context.services.animationRest = new RestFulController({
-      router,
-      mongodb: app.context.services.mongodb,
-      dbName: 'animations',
-      coll: 'animations',
-      path: '/danke/animation',
-      filter: app.middlewares.loginRequired
-    })
-    app.context.services.animationRest.setAdmin(config.admin)
-    app.context.services.workLinkServie = new RestFulController({
-      router,
-      mongodb: app.context.services.mongodb,
-      dbName: 'danke',
-      coll: 'links',
-      path: '/danke/link',
-      filter: app.middlewares.loginRequired
-    })
-    app.context.services.workLinkServie.setAdmin(config.admin)
-    app.context.services.vectorService = new RestFulController({
-      router,
-      mongodb: app.context.services.mongodb,
-      dbName: 'danke',
-      coll: 'vectors',
-      path: '/danke/vector',
-      filter: app.middlewares.loginRequired
-    })
-    app.context.services.vectorService.setAdmin(config.admin)
+    initRestService(app, 'danke', 'works', 'danke/work')
+    initRestService(app, 'danke', 'animations', 'danke/animation')
+    initRestService(app, 'danke', 'vectors', 'danke/vector')
+    initRestService(app, 'danke', 'audios', 'danke/audio')
   }
 }
